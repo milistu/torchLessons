0916fbe888bb381e9adfff66f15fcad4d8d83919
Milutin Studen <studen.m98@gmail.com>
2023-01-27 21:22:18 +0100

    Optimizing Model Parameters finish

--- PyTorch Tutorials/optModelParam.py
+++ PyTorch Tutorials/optModelParam.py
@@ -4,6 +4,7 @@ from torch import nn
 from torch.utils.data import DataLoader
 from torchvision import datasets
 from torchvision.transforms import ToTensor
+from tqdm import tqdm
 
 ## Prerequisite Code
 
@@ -57,8 +58,54 @@ We define the following hyperparameters for training:
 
 learning_rate = 1e-3
 batch_size = 64
-epochs = 5
+epochs = 10
 
 # Initialize the loss function
+loss_fn = nn.CrossEntropyLoss()
+
+# Initialize the optimizer
+optimizer = torch.optim.SGD(model.parameters(), lr=learning_rate)
+
+def train_loop(dataloader, model, loss_fn, optimizer):
+    size = len(dataloader.dataset)
+    for batch, (X,y) in enumerate(dataloader):
+        # Compute prediction and loss
+        pred = model(X)
+        loss = loss_fn(pred, y)
+
+        # Backpropagation
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+
+        if batch % 100 == 0:
+            loss, current = loss.item(), batch * len(X)
+            print(f"Loss: {loss:>7f} [{current:>5d}/{size:>5d}]")
+
+def test_loop(dataloader, model, loss_fn):
+    size = len(dataloader.dataset)
+    num_batches = len(dataloader)
+    test_loss, correct = 0, 0
+
+    with torch.no_grad():
+        for X, y in dataloader:
+            pred = model(X)
+            test_loss += loss_fn(pred, y).item()
+            correct += (pred.argmax(1) == y).type(torch.float).sum().item()
+
+    test_loss /= num_batches
+    correct /= size
+    print(f"Test Error: \n Accuracy: {(100*correct):>0.1f}%, Avg loss: {test_loss:>8f} \n")
+
+
+if __name__ == "__main__":
+    for t in tqdm(range(epochs)):
+        print(f"Epoch {t+1}\n--------------------------------")
+        train_loop(train_dataloader, model, loss_fn, optimizer)
+        test_loop(test_dataloader, model, loss_fn)
+
+    print("Done!")
+
+
 
 
