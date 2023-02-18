83eeb728d29118a0be41c0c972a517d33d9acff3
Milutin Studen <studen.m98@gmail.com>
2023-02-18 22:28:23 +0100

    Added parameters

--- Reinforcement Learning/robotWalk.py
+++ Reinforcement Learning/robotWalk.py
@@ -5,3 +5,19 @@ import torch as th
 
 from stable_baselines3 import PPO
 from stable_baselines3.common.evaluation import evaluate_policy
+
+env = gym.make("AntBulletEnv-v0")
+env.render(mode="human")
+
+MAX_AVERAGE_SCORE = 271
+
+policy_kwargs = dict(activation_fn = th.nn.LeakyReLU, net_arch = [512, 512])
+model = PPO('MlpPolicy', env, learning_rate=0.0003, policy_kwargs=policy_kwargs, verbose=1)
+
+for i in range(8000):
+    print("Training itteration: ", i)
+    model.learn(total_timesteps=10000)
+    model.save("ppo_Ant_saved_model")
+    mean_reward, std_reward = evaluate_policy(model, model.get_env(), n_eval_episodes=5)
+    
+
