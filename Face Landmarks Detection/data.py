a4c99c5e2423bfb52b5e78b16abd3eef1f95af24
Milutin Studen <studen.m98@gmail.com>
2023-02-21 22:26:22 +0100

    Added crop and call in Transforms class

--- Face Landmarks Detection/data.py
+++ Face Landmarks Detection/data.py
@@ -61,8 +61,31 @@ class Transforms():
         image = color_jitter(image)
         return image, landmarks
     
+    def crop_face(self, image, landmarks, crops):
+        left = int(crops['left'])
+        top = int(crops['top'])
+        width = int(crops['width'])
+        height = int(crops['height'])
 
+        image = TF.crop(image, top, left, height, width)
 
+        img_shape = np.array(image).shape
+        landmarks = torch.tensor(landmarks) - torch.tensor([[left, top]])
+        landmarks = landmarks / torch.tensor([img_shape[1], img_shape[0]]) # ???
+
+        return image, landmarks
+    
+    def __call__(self, image, landmarks, crops):
+        image = Image.fromarray(image)
+        image, landmarks = self.crop_face(image, landmarks, crops)
+        image, landmarks = self.resize(image, landmarks, (224, 224))
+        image, landmarks = self.color_jitter(image, landmarks)
+        image, landmarks = self.rotate(image, landmarks, angle=10)
+
+        image = TF.to_tensor(image)
+        image = TF.normalize(image, [0.5], [0.5])
+
+        return image, landmarks
 
 if __name__ == '__main__':
     getData()
