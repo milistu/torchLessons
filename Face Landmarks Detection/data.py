67b8ba03b71595d4f450883358d6bd98ac2143ed
Milutin Studen <studen.m98@gmail.com>
2023-02-22 18:01:27 +0100

    Added Dataset Class

--- Face Landmarks Detection/data.py
+++ Face Landmarks Detection/data.py
@@ -2,13 +2,16 @@ import os
 import random
 import urllib.request
 import tarfile
+import xml.etree.ElementTree as ET
 
+import cv2
 import numpy as np
 import imutils
 from PIL import Image
 import torch
 import torchvision.transforms.functional as TF
 from torchvision import datasets, models, transforms
+from torch.utils.data import Dataset, DataLoader
 
 def getData():
     print(f"[INFO] Current Working Directory: {os.getcwd()}")
@@ -28,6 +31,9 @@ def getData():
         os.remove(file_name)
     else:
         print(f"[INFO] Dataset already present")
+
+def visualizeData():
+    pass
 
 class Transforms():
     def __init__(self): pass
@@ -71,7 +77,7 @@ class Transforms():
 
         img_shape = np.array(image).shape
         landmarks = torch.tensor(landmarks) - torch.tensor([[left, top]])
-        landmarks = landmarks / torch.tensor([img_shape[1], img_shape[0]]) # ???
+        landmarks = landmarks / torch.tensor([img_shape[1], img_shape[0]]) 
 
         return image, landmarks
     
@@ -86,8 +92,55 @@ class Transforms():
         image = TF.normalize(image, [0.5], [0.5])
 
         return image, landmarks
+    
+class FaceLandmarksDataset(Dataset):
+    
+    def __init__(self, transform=None):
+
+        tree = ET.parse('data/ibug_300W_large_face_landmark_dataset/labels_ibug_300W_train.xml')
+        root = tree.getroot()
+
+        self.image_filenames = []
+        self.landmarks = []
+        self.crops = []
+        self.transform = transform
+        self.root_dir = 'data/ibug_300W_large_face_landmark_dataset/labels_ibug_300W_train.xml'
+
+        # Acces data info 
+        for filename in root[2]:
+            # Get image paths
+            self.image_filenames.append(os.path.join(self.root_dir, filename.attrib['file']))
+            # Get crop data
+            self.crops.append(filename[0].attrib)
+            # Get landmark data
+            landmark = []
+            for num in range(68):
+                x_coordinate = int(filename[0][num].attrib['x'])
+                y_coordinate = int(filename[0][num].attrib['y'])
+                landmark.append([x_coordinate, y_coordinate])
+            self.landmarks.append(landmark)
+
+        self.landmarks = np.array(self.landmarks, dtype=np.float32)
+
+        assert len(self.image_filenames) == len(self.landmarks)
+
+    def __len__(self):
+        return len(self.image_filenames)
+    
+    def __getitem__(self, index):
+        image = cv2.imread(self.image_filenames[index], 0)
+        landmarks = self.landmarks[index]
+
+        if self.transform:
+            image, landmarks = self.transform(image, landmarks, self.crops[index])
+        
+        # Zero-centre the landmarks - easier for NN to learn
+        landmarks = landmarks - 0.5
+
+        return image, landmarks
+
 
 if __name__ == '__main__':
     getData()
-
+    # Add data example
 
