8c6eafe993b2f25525a2150e8dbe5969912e07ee
Milutin Studen <studen.m98@gmail.com>
2023-03-02 19:33:48 +0100

    Replace cv2 bbox face detector with yoloface

--- Face_Landmarks_Detection/inferenceLive.py
+++ Face_Landmarks_Detection/inferenceLive.py
@@ -10,6 +10,8 @@ import torchvision.transforms.functional as TF
 import data
 import network as net
 
+from yoloface import face_analysis
+
 
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 print(f"🎰 Device used: {device}")
@@ -20,6 +22,7 @@ best_model.load_state_dict(torch.load('Face_Landmarks_Detection/weights/face_lan
 # best_model.to(device)
 
 faces_cascade = cv2.CascadeClassifier('Face_Landmarks_Detection/weights/haarcascade_frontalface_default.xml')
+faces_yolo = face_analysis()
 
 
 cap = cv2.VideoCapture(0)
@@ -30,12 +33,20 @@ while(True):
         grayscale_image = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
         # display_image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
         height, width, _ = frame.shape
+        # print(f"Width: {width} \nHeight: {height}")
 
         faces = faces_cascade.detectMultiScale(grayscale_image, 1.1, 4)
+        _, box, conf = faces_yolo.face_detection(frame_arr=frame, frame_status=True, model='tiny')
+        frame = faces_yolo.show_output(frame, box, frame_status=True)
 
+        print(box)
         all_landmarks = []
-        for (x, y, w, h) in faces:
-            image = grayscale_image[y:y+h, x:x+w]
+        for (x, y, w, h) in box:
+            # image = grayscale_image[y:y+h, x:x+w]
+            image = grayscale_image[y:y+w, x:x+h]
+            print(grayscale_image.shape)
+            # img_test = image.copy()
+
             image = TF.resize(Image.fromarray(image), size=(224, 224))
             image = TF.to_tensor(image)
             image = TF.normalize(image, [0.5], [0.5])
@@ -45,7 +56,8 @@ while(True):
             with torch.no_grad():
                 landmarks = best_model(image.unsqueeze(0))
             
-            landmarks = (landmarks.view(68,2).cpu().numpy() + 0.5) * np.array([[w, h]]) + np.array([[x, y]])
+            landmarks = (landmarks.view(68,2).cpu().numpy() + 0.5) * np.array([[h, w]]) + np.array([[x, y]])
+            # landmarks = (landmarks.view(68,2).cpu().numpy() + 0.5) * np.array([[w, h]]) + np.array([[x, y]])
             all_landmarks.append(landmarks)
         
         for landmarks in all_landmarks:
