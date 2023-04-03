1370fdd69fe2c80a48d0ff3ef0f30442d2b444a8
Milutin Studen <studen.m98@gmail.com>
2023-04-03 22:53:08 +0200

    Updated network architecture

--- classifier/nn.py
+++ classifier/nn.py
@@ -14,7 +14,10 @@ class Net(nn.Module):
         # print(resnet18)
         self.resnet18_core = nn.Sequential(*(list(resnet18.children())[1:-4]))
         self.avgpool = nn.AdaptiveAvgPool2d((1,1))
-        self.dropout = nn.Dropout(p = 0.5)
+        self.dropout = nn.Dropout(p = 0.2)
+
+        self.relu = nn.ReLU()
+        # self.softmax = nn.Softmax(10)
 
         self.fc1 = nn.Linear(128, 64)
         self.fc2 = nn.Linear(64, 10)
@@ -25,8 +28,11 @@ class Net(nn.Module):
         x = self.avgpool(x)
         x = x.view(x.size(0), -1)
         x = self.dropout(x)
-        x = self.fc1(x)
-        x = self.fc2(x)
+        # x = self.fc1(x)
+        # x = self.fc2(x)
+        x = self.relu(self.fc1(x))
+        x = self.relu(self.fc2(x))
+        # x = self.softmax(x)
 
         return x 
     
